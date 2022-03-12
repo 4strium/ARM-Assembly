970c039dc481ab6b57c76dfb769a80917b97081b
Astrium <romain.mellaza@orange.fr>
2022-03-12 19:12:19 +0100

    Update Fibonacci_loop_while.asm
    Add comments to the code

--- Fibonacci_loop_while.asm
+++ Fibonacci_loop_while.asm
@@ -6,7 +6,19 @@
 // $$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$  __$$ |$$ |$$ |  $$ |      $$ |\$  /$$ |    
 // $$ |  $$ |\$$$$$$  |$$ | $$ | $$ |\$$$$$$$ |$$ |$$ |  $$ |      $$ | \_/ $$ |$$\ 
 // \__|  \__| \______/ \__| \__| \__| \_______|\__|\__|  \__|      \__|     \__|\__|
-                                                                                                                                                                
+//
+// Program that calculates the sequence of "n" numbers (given in input) of the 
+// Fibonacci sequence by saving them from memory address 100.
+//
+// Registers R0, R1 and R2 are the registers used for the calculations of the sequence 
+// and whose results are directly stored in the memory by the command “STR”. 
+//
+// Register R3 is the memory address counter, it starts at address 102 because 
+// there are first 0 and 1 which are the starting values of the registers.      
+//
+// Finally, Register R4 temporarily stores the final value entered by the user, 
+// and compares it with the memory address counter (R3).      
+//                                                                                                                                                   
     INP R4,2
     ADD R4,R4,#100
     MOV R3,#102
